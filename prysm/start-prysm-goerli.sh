e58611d4a96eb8f12c7860fe7b477a813b1d34a8
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-04-11 16:03:42 +0530

    Update start-prysm-goerli.sh

--- prysm/start-prysm-goerli.sh
+++ prysm/start-prysm-goerli.sh
@@ -1,3 +1,3 @@
-export CHECKPOINT_SYNC_URL=https://goerli.checkpoint-sync.ethdevops.io
+export CHECKPOINT_SYNC_URL=https://beaconstate-goerli.chainsafe.io
 ./prysm.sh beacon-chain --prater --execution-endpoint=http://localhost:8551 --datadir=prysm --jwt-secret=/tmp/jwtsecret \
---checkpoint-sync-url=CHECKPOINT_SYNC_URL --genesis-beacon-api-url=CHECKPOINT_SYNC_URL
+--checkpoint-sync-url=$CHECKPOINT_SYNC_URL --genesis-beacon-api-url=$CHECKPOINT_SYNC_URL
